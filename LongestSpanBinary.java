141bdd065dda33764c0382ebca8cab1741efde44
kaustubh0078 <kaustubh0078@gmail.com>
2025-06-07 16:57:57 +0530

    Initial commit

---
+++ LongestSpanBinary.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LongestSpanBinary {
+    public int longestCommonSum(int[] a1, int[] a2) {
+        int n = a1.length;
+        int[] diff = new int[n];
+        for (int i = 0; i < n; i++) diff[i] = a1[i] - a2[i];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxLen = 0, prefixSum = 0;
+        for (int i = 0; i < n; i++) {
+            prefixSum += diff[i];
+            if (prefixSum == 0) maxLen = i + 1;
+            else if (map.containsKey(prefixSum)) {
+                maxLen = Math.max(maxLen, i - map.get(prefixSum));
+            } else {
+                map.put(prefixSum, i);
+            }
+        }
+        return maxLen;
+    }
+}
